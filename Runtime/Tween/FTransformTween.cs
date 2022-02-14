38209361140a2559f3859056143e31fdc3d6fae6
Navid Bigdeli <navidbigdeli54@gmail.com>
2022-02-14 17:51:23 +0330

    added tween's transform extenstion

---
+++ Runtime/Tween/FTransformTween.cs
@@ -0,0 +1,54 @@
+/**Copyright 2016 - 2022, Dream Machine Game Studio. All Right Reserved.*/
+
+using UnityEngine;
+using System.Collections;
+
+namespace DreamMachineGameStudio.Dreamworks.Tween
+{
+    public static partial class FTween
+    {
+        public static void DoMove(this Transform transform, Vector3 endPosition, float duration)
+        {
+            _coroutine.Start(Move(transform, endPosition, duration));
+        }
+
+        public static void DoLocalMove(this Transform transform, Vector2 endPosition, float duration)
+        {
+            _coroutine.Start(LocalMove(transform, endPosition, duration));
+        }
+
+        private static IEnumerator Move(Transform transform, Vector3 endPosition, float duration)
+        {
+            Vector3 startPosition = transform.position;
+
+            float elapsedTime = 0;
+
+            while (elapsedTime < duration)
+            {
+                transform.position = Vector3.Lerp(startPosition, endPosition, elapsedTime / duration);
+
+                yield return null;
+
+                elapsedTime += Time.deltaTime;
+            }
+        }
+
+        private static IEnumerator LocalMove(Transform transform, Vector3 endPosition, float duration)
+        {
+            Vector3 startPosition = transform.localPosition;
+
+            float elapsedTime = 0;
+
+            while (elapsedTime < duration)
+            {
+                transform.localPosition = Vector3.Lerp(startPosition, endPosition, elapsedTime / duration);
+
+                yield return _waitForEndOfFrame;
+
+                elapsedTime += Time.deltaTime;
+            }
+
+            transform.localPosition = endPosition;
+        }
+    }
+}
---
+++ Runtime/Tween/FTween.cs
@@ -0,0 +1,22 @@
+/**Copyright 2016 - 2022, Dream Machine Game Studio. All Right Reserved.*/
+
+using UnityEngine;
+
+namespace DreamMachineGameStudio.Dreamworks.Tween
+{
+    public static partial class FTween
+    {
+        #region Fields
+        private readonly static Coroutine.ICoroutine _coroutine;
+
+        private readonly static WaitForEndOfFrame _waitForEndOfFrame = new WaitForEndOfFrame();
+        #endregion
+
+        #region Constructors
+        static FTween()
+        {
+            _coroutine = new GameObject("Tweener").AddComponent<Coroutine.CCoroutine>();
+        }
+        #endregion
+    }
+}
