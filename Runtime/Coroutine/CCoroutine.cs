e53ca84debd788404efdcab81e3312db5a5da73d
Navid Bigdeli <navidbigdeli54@gmail.com>
2022-02-14 17:50:54 +0330

    Added coroutine system

---
+++ Runtime/Coroutine/CCoroutine.cs
@@ -0,0 +1,38 @@
+﻿/**Copyright 2016 - 2022, Dream Machine Game Studio. All Right Reserved.*/
+
+using UnityEngine;
+
+namespace DreamMachineGameStudio.Dreamworks.Coroutine
+{
+    public class CCoroutine : MonoBehaviour, ICoroutine
+    {
+        #region MonoBehaviour Methods
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+        #endregion
+
+        #region ICoroutineManagement Implementation
+        UnityEngine.Coroutine ICoroutine.Start(System.Collections.IEnumerator coroutine)
+        {
+            return StartCoroutine(coroutine);
+        }
+
+        void ICoroutine.Stop(System.Collections.IEnumerator coroutine)
+        {
+            StopCoroutine(coroutine);
+        }
+
+        void ICoroutine.Stop(UnityEngine.Coroutine coroutine)
+        {
+            StopCoroutine(coroutine);
+        }
+
+        void ICoroutine.StopAll()
+        {
+            StopAllCoroutines();
+        }
+        #endregion
+    }
+}
---
+++ Runtime/Coroutine/ICoroutine.cs
@@ -0,0 +1,22 @@
+﻿/**Copyright 2016 - 2022, Dream Machine Game Studio. All Right Reserved.*/
+
+using System.Collections;
+
+namespace DreamMachineGameStudio.Dreamworks.Coroutine
+{
+    /// <summary>
+    /// CoroutineManagement is responsible for running coroutines for non-monobehaviour classes.
+    /// </summary>
+    public interface ICoroutine
+    {
+        #region Methods
+        UnityEngine.Coroutine Start(IEnumerator coroutine);
+
+        void Stop(IEnumerator coroutine);
+
+        void Stop(UnityEngine.Coroutine coroutine);
+
+        void StopAll();
+        #endregion
+    }
+}
