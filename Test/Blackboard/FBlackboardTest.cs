b662b69ca535890bcefd8492bd9e57d71546ab6f
Navid Bigdeli <navidbigdeli54@gmail.com>
2022-05-16 21:58:27 +0430

    added two tests for FBlackboard

---
+++ Test/Blackboard/FBlackboardTest.cs
@@ -0,0 +1,40 @@
+﻿/**Copyright 2016 - 2022, Dream Machine Game Studio. All Right Reserved.*/
+
+using System;
+using NUnit.Framework;
+using DreamMachineGameStudio.Dreamworks.Debug;
+using DreamMachineGameStudio.Dreamworks.Variant;
+using DreamMachineGameStudio.Dreamworks.Blackboard;
+
+namespace DreamMachineGameStudio.Dreamworks.Test.Blackboard
+{
+    public class FBlackboardTest
+    {
+        [Test]
+        public void GetValueTest()
+        {
+            string key = Guid.NewGuid().ToString();
+            string value = Guid.NewGuid().ToString();
+
+            FBlackboard blackboard = new FBlackboard();
+            blackboard.AddValue(key, new FString(value));
+
+            FString variant = blackboard.GetValue<FString>(key);
+
+            FAssert.AreEqual(new FString(value), variant);
+        }
+
+        [Test]
+        public void TryGetValueTest()
+        {
+            string key = Guid.NewGuid().ToString();
+            string value = Guid.NewGuid().ToString();
+
+            FBlackboard blackboard = new FBlackboard();
+            blackboard.AddValue(key, new FString(value));
+
+            FAssert.IsTrue(blackboard.TryGetValue(key, out FString variant));
+            FAssert.AreEqual(new FString(value), variant);
+        }
+    }
+}
