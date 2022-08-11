34033159beb79e91da1d074226aa0cc2488f8a12
navidbigdeli54 <navidbigdeli54@gmail.com>
2022-08-12 02:33:01 +0430

    added singleton abstract classes

---
+++ Runtime/Core/CSingleton.cs
@@ -0,0 +1,28 @@
+﻿/**Copyright 2016 - 2022, Dream Machine Game Studio. All Right Reserved.*/
+
+using UnityEngine;
+
+namespace DreamMachineGameStudio.Dreamworks.Core
+{
+    public abstract class CSingleton<T> where T : MonoBehaviour
+    {
+        #region Fields
+        private static T _instance;
+        #endregion
+
+        #region Properties
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new GameObject(typeof(T).Name).AddComponent<T>();
+                }
+
+                return _instance;
+            }
+        }
+        #endregion
+    }
+}
---
+++ Runtime/Core/FSingleton.cs
@@ -0,0 +1,28 @@
+﻿/**Copyright 2016 - 2022, Dream Machine Game Studio. All Right Reserved.*/
+
+namespace DreamMachineGameStudio.Dreamworks.Core
+{
+    public abstract class FSingleton<T> where T : class, new()
+    {
+        #region Fields
+        private static readonly T _instance;
+        #endregion
+
+        #region Constructors
+        static FSingleton()
+        {
+            _instance = new T();
+        }
+        #endregion
+
+        #region Properties
+        public static T Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+        #endregion
+    }
+}
