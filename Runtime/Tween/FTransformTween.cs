327214e003e0c399537c2540088ab4449bb290e0
navidbigdeli54 <navidbigdeli54@gmail.com>
2023-01-13 17:11:33 +0330

    added DoRotate tween

--- Runtime/Tween/FTransformTween.cs
+++ Runtime/Tween/FTransformTween.cs
@@ -1,5 +1,6 @@
 /**Copyright 2016 - 2022, Dream Machine Game Studio. All Right Reserved.*/
 
+using System;
 using UnityEngine;
 using System.Collections;
 
@@ -7,6 +8,7 @@ namespace DreamMachineGameStudio.Dreamworks.Tween
 {
     public static partial class FTween
     {
+        #region Public Methods
         public static void DoMove(this Transform transform, Vector3 endPosition, float duration)
         {
             _coroutine.Start(Move(transform, endPosition, duration));
@@ -17,6 +19,18 @@ namespace DreamMachineGameStudio.Dreamworks.Tween
             _coroutine.Start(LocalMove(transform, endPosition, duration));
         }
 
+        public static void DoRotate(this Transform transform, Quaternion endRotation, float duration)
+        {
+            _coroutine.Start(Rotate(transform, endRotation, duration, null));
+        }
+
+        public static void DoRotate(this Transform transform, Quaternion endRotation, float duration, Action callback)
+        {
+            _coroutine.Start(Rotate(transform, endRotation, duration, callback));
+        }
+        #endregion
+
+        #region Private Methods
         private static IEnumerator Move(Transform transform, Vector3 endPosition, float duration)
         {
             Vector3 startPosition = transform.position;
@@ -50,5 +64,26 @@ namespace DreamMachineGameStudio.Dreamworks.Tween
 
             transform.localPosition = endPosition;
         }
+
+        private static IEnumerator Rotate(Transform transform, Quaternion endRotation, float duration, Action callback)
+        {
+            Quaternion startRotation = transform.rotation;
+
+            float elapsedTime = 0;
+
+            while (elapsedTime < duration)
+            {
+                transform.rotation = Quaternion.Lerp(startRotation, endRotation, elapsedTime / duration);
+
+                yield return _waitForEndOfFrame;
+
+                elapsedTime += Time.deltaTime;
+            }
+
+            transform.rotation = endRotation;
+
+            callback?.Invoke();
+        }
+        #endregion
     }
 }
