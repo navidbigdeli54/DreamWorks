a238a38eb7f4bfc42de58a74b6a6f3a95a43d7a8
navidbigdeli54 <navidbigdeli54@gmail.com>
2023-04-14 19:21:12 +0430

    Added IsEmpty and IsNotEmpty extensions for ICollection<T>

---
+++ Runtime/Extension/FICollectionExtension.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace DreamMachineGameStudio.Dreamworks.Extension
+{
+    public static class FICollectionExtension
+    {
+        public static bool IsEmpty<T>(this ICollection<T> collection)
+        {
+            return collection.Count == 0;
+        }
+
+        public static bool IsNotEmpty<T>(this ICollection<T> collection)
+        {
+            return !collection.IsEmpty();
+        }
+    }
+}
