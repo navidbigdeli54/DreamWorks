cf18ae3db6b773afedad3dfcb57cb39e3ec2b0d2
navidbigdeli54 <navidbigdeli54@gmail.com>
2023-05-04 15:58:41 +0430

    Overrided equal and not equal methods for the FPoolableInstanceOwner.

--- Runtime/ObjectPool/FPoolableInstanceOwner.cs
+++ Runtime/ObjectPool/FPoolableInstanceOwner.cs
@@ -2,11 +2,37 @@ namespace DreamMachineGameStudio.Dreamworks.ObjectPool
 {
     public struct FPoolableInstanceOwner
     {
+        #region Properties
         public string Name { get; }
+        #endregion
 
+        #region Constructors
         public FPoolableInstanceOwner(string name)
         {
             Name = name;
         }
+        #endregion
+
+        #region Public Methods
+        public override bool Equals(object obj)
+        {
+            return obj is FPoolableInstanceOwner owner && Name == owner.Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+
+        public static bool operator ==(FPoolableInstanceOwner lfs, FPoolableInstanceOwner rhs)
+        {
+            return lfs.Name == rhs.Name;
+        }
+
+        public static bool operator !=(FPoolableInstanceOwner lhs, FPoolableInstanceOwner rhs)
+        {
+            return lhs.Name != rhs.Name;
+        } 
+        #endregion
     }
 }
