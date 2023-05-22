682409d44654a4056cee30936f96446b61af9426
navidbigdeli54 <navidbigdeli54@gmail.com>
2023-05-22 08:47:08 +0330

    Added IAnalytics interface.

---
+++ Runtime/Analytics/IAnalytics.cs
@@ -0,0 +1,11 @@
+/**Copyright 2016 - 2023, Dream Machine Game Studio. All Right Reserved.*/
+
+using DreamMachineGameStudio.Dreamworks.Core;
+
+namespace DreamMachineGameStudio.Dreamworks.Analytics
+{
+    public interface IAnalytics : IService
+    {
+
+    }
+}
