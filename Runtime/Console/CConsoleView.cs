1663b5daa4e0fc85f1570496a47027678dc2e705
Navid Bigdeli <navidbigdeli54@gmail.com>
2024-01-27 23:42:41 +0330

    Changed console view to be able to use it in mobile devices.

--- Runtime/Console/CConsoleView.cs
+++ Runtime/Console/CConsoleView.cs
@@ -1,68 +1,101 @@
 using UnityEngine;
 using System.Threading.Tasks;
 using DreamMachineGameStudio.Dreamworks.Core;
+using DreamMachineGameStudio.Dreamworks.Debug;
 
 namespace DreamMachineGameStudio.Dreamworks.Console
 {
-    public class CConsoleView : CComponent
-    {
-        #region Fields
-        private const int CONSOLE_HEIGHT = 40;
+	public class CConsoleView : CComponent
+	{
+		#region Fields
+		private const int CONSOLE_HEIGHT = 40;
 
-        private bool _isActive;
+		private bool _isActive;
 
-        private string _inputText;
+		private string _inputText;
 
-        private GUISkin _skin;
-        #endregion
+		private GUISkin _skin;
+		#endregion
 
-        #region CComponent Methods
-        protected override async Task PreInitializeComponenetAsync()
-        {
-            await base.PreInitializeComponenetAsync();
+		#region CComponent Methods
+		protected override async Task PreInitializeComponenetAsync()
+		{
+			await base.PreInitializeComponenetAsync();
 
-            _skin = Resources.Load<GUISkin>("console_skin");
+			CanEverTick = true;
 
-            MakePersistent();
-        }
-        #endregion
+			_skin = Resources.Load<GUISkin>("console_skin");
 
-        #region MonoBehaviour Methods
-        private void OnGUI()
-        {
-            Event e = Event.current;
-            if (e.type == EventType.KeyDown)
-            {
-                if (e.keyCode == KeyCode.BackQuote)
-                {
-                    _isActive = !_isActive;
-                    _inputText = string.Empty;
-                }
-                else if (_isActive && e.keyCode == KeyCode.Return)
-                {
-                    _isActive = false;
-                    FConsole.Instance.ExecuteCommand(_inputText);
-                    _inputText = string.Empty;
-                }
-            }
+			MakePersistent();
+		}
 
-            if (_isActive)
-            {
-                GUILayout.BeginVertical(GUILayout.Width(Screen.width), GUILayout.Height(CONSOLE_HEIGHT));
-                GUILayout.Space(Screen.height - CONSOLE_HEIGHT);
-                GUILayout.BeginHorizontal();
-                GUI.SetNextControlName(nameof(_inputText));
-                _inputText = GUILayout.TextField(_inputText, _skin.textField, GUILayout.Height(CONSOLE_HEIGHT));
-                GUI.FocusControl(nameof(_inputText));
-                GUILayout.EndHorizontal();
-                GUILayout.EndVertical();
-            }
+		protected override void TickComponent(float deltaTime)
+		{
+			base.TickComponent(deltaTime);
 
-            if (e.keyCode == KeyCode.BackQuote)
-            {
-                _inputText = string.Empty;
-            }
-        }
-        #endregion
-    }
+			if (Input.GetKeyDown(KeyCode.BackQuote) || Input.touchCount > 2)
+			{
+				Activate();
+			}
+		}
+		#endregion
+
+		#region MonoBehaviour Methods
+		private void OnGUI()
+		{
+			if (_isActive)
+			{
+				Event e = Event.current;
+				if (e.type == EventType.KeyDown)
+				{
+					if (e.keyCode == KeyCode.BackQuote)
+					{
+						Deactivate();
+
+						return;
+					}
+					else if (e.keyCode == KeyCode.Return || e.keyCode == KeyCode.KeypadEnter)
+					{
+						Submit();
+
+						return;
+					}
+				}
+
+				GUILayout.BeginVertical(GUILayout.Width(Screen.width), GUILayout.Height(CONSOLE_HEIGHT * Screen.dpi));
+				GUILayout.Space(Screen.height - CONSOLE_HEIGHT);
+				GUILayout.BeginHorizontal();
+				GUI.SetNextControlName(nameof(_inputText));
+				_inputText = GUILayout.TextField(_inputText, _skin.textField, GUILayout.Height(CONSOLE_HEIGHT));
+				GUI.FocusControl(nameof(_inputText));
+				if (GUILayout.Button("Enter", GUILayout.Height(CONSOLE_HEIGHT), GUILayout.Width(100)))
+				{
+					Submit();
+				}
+				GUILayout.EndHorizontal();
+				GUILayout.EndVertical();
+			}
+		}
+		#endregion
+
+		#region Private Methods
+		private void Activate()
+		{
+			_isActive = true;
+			_inputText = string.Empty;
+		}
+
+		private void Deactivate()
+		{
+			_isActive = false;
+		}
+
+		private void Submit()
+		{
+			Deactivate();
+
+			FConsole.Instance.ExecuteCommand(_inputText);
+		}
+		#endregion
+	}
 }
--- Runtime/Console/FConsole.cs
+++ Runtime/Console/FConsole.cs
@@ -4,43 +4,42 @@ using DreamMachineGameStudio.Dreamworks.Core;
 
 namespace DreamMachineGameStudio.Dreamworks.Console
 {
-    public class FConsole : FSingleton<FConsole>
-    {
-        #region Fields
-        private static Dictionary<string, FCommand> _commands = new Dictionary<string, FCommand>();
+	public class FConsole : FSingleton<FConsole>
+	{
+		#region Fields
+		private static Dictionary<string, FCommand> _commands = new Dictionary<string, FCommand>();
 
-        private static FDefaultCommands defualtCommands;
-        #endregion
+		private static FDefaultCommands defualtCommands = new FDefaultCommands();
+		#endregion
 
-        #region Constructors
-        static FConsole()
-        {
-            defualtCommands = new FDefaultCommands();
-            FConsoleUtility.TryAddCommands(defualtCommands);
-        }
-        #endregion
+		#region Constructors
+		private FConsole()
+		{
+			
+		}
+		#endregion
 
-        #region Public Methods
-        public void AddCommand(FCommand command)
-        {
-            _commands.Add(command.Name.ToLower(), command);
-        }
+		#region Public Methods
+		public void AddCommand(FCommand command)
+		{
+			_commands.Add(command.Name.ToLower(), command);
+		}
 
-        public void RemoveCommand(FCommand command)
-        {
-            _commands.Remove(command.Name.ToLower());
-        }
+		public void RemoveCommand(FCommand command)
+		{
+			_commands.Remove(command.Name.ToLower());
+		}
 
-        public void ExecuteCommand(string commandString)
-        {
-            string[] splittedCommandString = commandString.Split(' ');
+		public void ExecuteCommand(string commandString)
+		{
+			string[] splittedCommandString = commandString.Split(' ');
 
-            string commandName = splittedCommandString[0];
-            if (_commands.TryGetValue(commandName.ToLower(), out FCommand command))
-            {
-                command.Execute(splittedCommandString.TakeLast(splittedCommandString.Length - 1).ToArray());
-            }
-        }
-        #endregion
-    }
+			string commandName = splittedCommandString[0];
+			if (_commands.TryGetValue(commandName.ToLower(), out FCommand command))
+			{
+				command.Execute(splittedCommandString.TakeLast(splittedCommandString.Length - 1).ToArray());
+			}
+		}
+		#endregion
+	}
 }
