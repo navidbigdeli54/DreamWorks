0ebc67835f4769b6a092e3d508f4cbe6b4ef8da8
Navid Bigdeli <navidbigdeli54@gmail.com>
2024-12-02 08:22:57 +0330

    Added IsValidIndex extension.

--- Runtime/Extension/FICollectionExtension.cs
+++ Runtime/Extension/FICollectionExtension.cs
@@ -3,15 +3,20 @@
 namespace DreamMachineGameStudio.Dreamworks.Extension.Collection
 {
 	public static class FICollectionExtension
-    {
-        public static bool IsEmpty<T>(this ICollection<T> collection)
-        {
-            return collection.Count == 0;
-        }
+	{
+		public static bool IsEmpty<T>(this ICollection<T> collection)
+		{
+			return collection.Count == 0;
+		}
 
-        public static bool IsNotEmpty<T>(this ICollection<T> collection)
-        {
-            return !collection.IsEmpty();
-        }
-    }
+		public static bool IsNotEmpty<T>(this ICollection<T> collection)
+		{
+			return !collection.IsEmpty();
+		}
+
+		public static bool IsValidIndex<T>(this ICollection<T> collection, int i)
+		{
+			return i >= 0 && collection.Count > i;
+		}
+	}
 }
